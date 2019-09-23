2424c4e5ee80a5a1beb8f8b99530fe0c0810fa05
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-23 16:24:57 -0400

    Initial commit

---
+++ CodeWars/Challenges/Isograms/solution.rb
@@ -0,0 +1,11 @@
+def is_isogram(string)
+    string.downcase!
+    ('a'..'z').each do |letter|
+      return false if string.count(letter) > 1
+    end
+    return true
+end
+  
+is_isogram("Dermaoglyphics")
+is_isogram("aba")
+is_isogram("moOse")
