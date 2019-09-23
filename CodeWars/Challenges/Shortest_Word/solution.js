8ccc24ea17a3bd7bc65320f714fe0b6796853789
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-23 14:49:52 -0400

    Initial commit

---
+++ CodeWars/Challenges/Shortest_Word/solution.js
@@ -0,0 +1,17 @@
+function findShort(s){
+
+    let array = s.split(' ');
+    console.log(array);
+    
+    let shortest = array[0].length;
+    
+    for(let i = 0; i < array.length; i++) {
+      if(shortest > array[i].length) {
+        shortest = array[i].length;
+      }
+    }
+    return shortest;
+}
+  
+findShort(("bitcoin take over the world maybe who knows perhaps"), 3);
+findShort(("turns out random test cases are easier than writing out basic ones"), 3);
