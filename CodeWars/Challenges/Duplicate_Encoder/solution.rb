cebee65c4b4e769df080b10b6e0d58b9f88f8ed0
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-23 16:05:08 -0400

    Initial commit

---
+++ CodeWars/Challenges/Duplicate_Encoder/solution.rb
@@ -0,0 +1,8 @@
+def duplicate_encode(word)
+    word.downcase.chars.map {|x| word.downcase.count(x) > 1 ? ")" : "("}.join("")
+end
+  
+duplicate_encode("din")
+duplicate_encode("recede")
+duplicate_encode("Success")
+duplicate_encode("(( @")
