fbedb0be713e5943314e3de6e523b629e9c57b9a
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-23 13:40:09 -0400

    Initial commit

---
+++ CodeWars/Challenges/Regex_Validate_PIN_Code/solution.js
@@ -0,0 +1,11 @@
+function validatePIN (pin) {
+    if (/^(\d{4}|\d{6})$/.test(pin)) {
+      return true;
+    } else {
+      return false;
+    }
+}
+  
+validatePIN("1234");
+validatePIN("12345");
+validatePIN("a234");
