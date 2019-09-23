a97b9f75b3b6134c57ab6cf2fd2f40bbf1110a3d
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-23 14:31:03 -0400

    Initial commit

---
+++ CodeWars/Challenges/Playing_with_Digits/solution.js
@@ -0,0 +1,22 @@
+function digPow(n, p) {
+
+    let digits = n.toString().split('');
+    let result = 0;
+    
+    for (let i = 0; i < digits.length; i++) {
+      result = result + Math.pow(digits[i], p);
+      p++;
+    }
+    
+    let data = result / n;
+    if(result % n === 0) {
+      return data;
+    } else {
+      return -1;
+    }
+}
+  
+digPow((89, 1), 1);
+digPow((92, 1), -1);
+digPow((695, 2), 2);
+digPow((46288, 3), 51);
