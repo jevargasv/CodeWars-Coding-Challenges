a2623aa8e2733d6597841552423d2d69fe0e0ea5
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-24 14:01:04 -0400

    Initial commit

---
+++ CodeWars/Challenges/Build_a_Pile_of_Cubes/solution.js
@@ -0,0 +1,16 @@
+function findNb(m) {
+    
+    for (let n = 0; ; n++) {
+      if (m > 0) {
+      let currentCubeVolume = Math.pow(n + 1, 3);
+      m = m - currentCubeVolume;
+      } else if (m == 0) {
+        return n;
+      } else if (m < 0) {
+        return (-1);
+      }
+   }
+}
+
+findNb(1071225);
+findNb(91716553919377);
