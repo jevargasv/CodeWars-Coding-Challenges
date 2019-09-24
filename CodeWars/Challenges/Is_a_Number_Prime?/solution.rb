f92641aaa941b3b8a433ccddb1988c6427651e02
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-24 15:49:34 -0400

    Initial commit

---
+++ CodeWars/Challenges/Is_a_Number_Prime?/solution.rb
@@ -0,0 +1,16 @@
+def isPrime(num)
+    if num < 2
+      return false
+    end
+    
+    for i in 2..(num - 1)
+      if (num % i) == 0
+      return false
+      end
+    end
+    return true
+end
+  
+isPrime(1)
+isPrime(0)
+isPrime(-1)
