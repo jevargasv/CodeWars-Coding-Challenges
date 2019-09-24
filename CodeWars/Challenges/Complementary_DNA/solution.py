19b2b86a27c6ece93543ef83462f810dbeb53ec7
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-24 16:07:09 -0400

    Initial commit

---
+++ CodeWars/Challenges/Complementary_DNA/solution.py
@@ -0,0 +1,12 @@
+def DNA_strand(dna):
+    complements = {
+      'A': 'T',
+      'T': 'A',
+      'G': 'C',
+      'C': 'G',
+    }
+    return ''.join([complements[c] for c in dna])
+    
+print(DNA_strand("AAAA"))
+print(DNA_strand("ATTGC"))
+print(DNA_strand("GTAT"))
