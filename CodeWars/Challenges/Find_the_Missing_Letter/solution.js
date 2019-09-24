5d7d0b2ec1ab4c10f405bb72e9e2be0468850eff
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-24 13:39:33 -0400

    Initial commit

---
+++ CodeWars/Challenges/Find_the_Missing_Letter/solution.js
@@ -0,0 +1,15 @@
+function findMissingLetter(array) {
+
+    let string = "abcdefghijklmnopqrstuvwxyz";
+    let stringToArray = array.join("");
+    for (let i = 0; i < stringToArray.length; i++) {
+      if (stringToArray.charCodeAt(i + 1) - stringToArray.charCodeAt(i) != 1) {
+      return String.fromCharCode(stringToArray.charCodeAt(i) + 1);
+      }
+      
+    }
+    
+}
+  
+findMissingLetter(['a','b','c','d','f']);
+findMissingLetter(['O','Q','R','S']);
